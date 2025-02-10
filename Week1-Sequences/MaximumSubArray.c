fc90023397a8cc4a7ea8fd679f77d935c605ea98
Rick-gogogo <you.huang@hotmail.com>
2025-02-10 13:39:24 -0800

    Update MaximumSubArray.c

--- Week1-Sequences/MaximumSubArray.c
+++ Week1-Sequences/MaximumSubArray.c
@@ -29,3 +29,15 @@ int maxSubArray(int* nums, int numsSize) {
     printf("largest_sum: %d",largest_sum);
     return largest_sum;
 }
+
+
+//The easiest way from answer
+int maxSubArray(int* nums, int numsSize) {
+    int max_sum = nums[0];  
+    int current_sum = nums[0]; 
+    for (int i = 1; i < numsSize; i++) {
+        current_sum = (current_sum + nums[i] > nums[i]) ? (current_sum + nums[i]) : nums[i];  
+        max_sum = (current_sum > max_sum) ? current_sum : max_sum; 
+    }
+    return max_sum;
+}
